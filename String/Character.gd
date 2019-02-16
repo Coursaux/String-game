a9e83047637b3fe788a106744bfdfcf04aad3243
Coursaux <corso77@live.com>
2019-02-16 15:37:44 -0700

    mouse

--- String/Character.gd
+++ String/Character.gd
@@ -1,18 +1,7 @@
 extends KinematicBody2D
 
-var motion = Vector2()
-
-func _physics_process(delta):
-	if Input.is_action_pressed("ui_right"):
-		motion.x = 200
-	elif Input.is_action_pressed("ui_left"):
-		motion.x = -200
-	elif Input.is_action_pressed("ui_up"):
-		motion.y = -200
-	elif Input.is_action_pressed("ui_down"):
-		motion.y = 200
-	else:
-		motion.x = 0
-		motion.y = 0
-		
-	motion = move_and_slide(motion)
+func _input(event):
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+   # Mouse in viewport coordinates
+	if event is InputEventMouseMotion:
+		set_position(event.position)
