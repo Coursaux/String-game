de65603678d990702a9fa7440f50ac278f6a6440
Austin Siew <glowing.locker@gmail.com>
2019-02-16 19:42:38 -0700

    sideways gravity mate

--- String/Character.gd
+++ String/Character.gd
@@ -2,7 +2,13 @@ extends KinematicBody2D
 
 func _input(event):
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-   # Mouse in viewport coordinates
+	# Mouse in viewport coordinates
 	if event is InputEventMouseMotion:
 		set_position(event.position)
-		print(event.position)
+
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(Vector2(0, 0))
+	if collision_info:
+		print(collision_info)
+		var collision_point = collision_info.position
