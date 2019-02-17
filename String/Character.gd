b47ff4a649cffbfb5e0c92a905363b8ff47fabe0
Austin Siew <glowing.locker@gmail.com>
2019-02-16 21:52:53 -0700

    atTEMPted to use relative mouse coords

--- String/Character.gd
+++ String/Character.gd
@@ -3,4 +3,7 @@ extends KinematicBody2D
 func _input(event):
    # Mouse in viewport coordinates
 	if event is InputEventMouseMotion:
-		set_position(event.position)
+		# set_position(event.position)
+		var collision = move_and_collide(event.relative)
+		if collision:
+			get_tree().quit()
--- String/Main.gd
+++ String/Main.gd
@@ -8,6 +8,10 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	# print(get_viewport().get_mouse_position())
+	#print()
+	#print(get_viewport().size.y)
+	get_viewport().warp_mouse(get_viewport().size / 2)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
@@ -17,6 +21,11 @@ func _ready():
 func _on_Timer_timeout():
 	spawn()
 	$Spawn.wait_time = rand_range(1, 2);
+	
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_quit"):
+		get_tree().quit()
 
 func spawn():
 	var obstacle = preload("res://Wall.tscn")
