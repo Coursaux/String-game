be28fee836c8d13b3b11800d7821a63a766445f1
Austin Siew <glowing.locker@gmail.com>
2019-02-17 13:13:37 -0700

    mouse works baby

--- String/Character.gd
+++ String/Character.gd
@@ -1,9 +1,38 @@
 extends KinematicBody2D
 
+
+# all for positions
+var absolute_override = Vector2(0, 0)
+var previous = Vector2(0, 0)
+var current = Vector2(0, 0)
+
+
 func _input(event):
    # Mouse in viewport coordinates
 	if event is InputEventMouseMotion:
-		# set_position(event.position)
-		var collision = move_and_collide(event.relative)
+		#relative = event.relative
+		current = event.position
+
+func set_mouse_position(custom_absolute):
+	absolute_override = custom_absolute
+
+func _physics_process(delta):
+	if absolute_override:
+		set_position(absolute_override)
+		#print("absolute_override", absolute_override)
+		previous = absolute_override
+		absolute_override = Vector2(0, 0)
+		print(previous)
+	
+	elif current:
+		var relative = current - previous
+		#print("relative", relative)
+		var collision = move_and_collide(relative)
 		if collision:
 			get_tree().quit()
+		previous = current
+		current = Vector2(0, 0)
+	
+	if is_on_ceiling() or is_on_wall() or is_on_floor():
+		print("epic gamer moment")
+
--- String/Main.gd
+++ String/Main.gd
@@ -5,13 +5,14 @@ extends Node
 # var b = "textvar"
 
 func _ready():
+	pass
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	# print(get_viewport().get_mouse_position())
-	#print()
-	#print(get_viewport().size.y)
-	get_viewport().warp_mouse(get_viewport().size / 2)
+	#Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+	var screen_center = get_viewport().size / 2
+	get_viewport().warp_mouse(screen_center)
+	$character.set_mouse_position(screen_center)
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
