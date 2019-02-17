8e8659e2d398ca23a920e1074d9c840a61be344d
Austin Siew <glowing.locker@gmail.com>
2019-02-16 20:56:32 -0700

    look at these guns c;

--- String/Wall.gd
+++ String/Wall.gd
@@ -1,6 +1,13 @@
 extends KinematicBody2D
 
+# chance = percentage / 10 (eg. 5.0 = 50%)
+var chance = 5.0
 var motion = Vector2()
+
+func _ready():
+	var i = rand_range(1, 10)
+	if i < chance:
+		print("it's rewind time")
 
 func _physics_process(delta):
 	motion.x = -200
