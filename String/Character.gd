270d718476c9e19c3862e092cfdf2a43a2156c4c
Mia2b <mia2b@hotmail.com>
2019-02-17 00:30:26 -0700

    Lots of changes that make a bungie cord thing

--- String/Character.gd
+++ String/Character.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 func _input(event):
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
    # Mouse in viewport coordinates
 	if event is InputEventMouseMotion:
 		set_position(event.position)
---
+++ String/Rope/Hook.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+const pin = preload("res://Rope/Pin/Pin.tscn")
+const link = preload("res://Rope/Link/Link.tscn")
+const box = preload("res://Rope/Box.tscn")
+
+export (int) var segments = 50
+
+func _ready():
+    var parent = $Anchor
+    for i in range (segments):
+        var child = addPin(parent)
+        addLink(parent, child)
+        parent = child
+    var child = addBox(parent)
+    addLink(parent, child)
+
+func addBox(parent):
+    var p = box.instance()
+    p.position = parent.position
+    p.position.y += 20
+    add_child(p)
+    return p
+
+func addPin(parent):
+    var p = pin.instance()
+    p.position = parent.position
+    p.position.y += 5
+    add_child(p)
+    return p
+
+func addLink(parent, child):
+    var p = link.instance()
+    p.node_a = parent.get_path()
+    p.node_b = child.get_path()
+    parent.add_child(p)
