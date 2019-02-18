b9fc90bdbb88913677556a5ad5d7995248175741
Coursaux <corso77@live.com>
2019-02-17 23:34:47 -0700

    fixes

--- String/Laser Finish.gd
+++ String/Laser Finish.gd
@@ -1,5 +1,13 @@
 extends KinematicBody2D
 
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.x = -1 
+	var collision = move_and_collide(motion * delta)
+	if collision:
+		get_tree().change_scene("res://MainMenu.tscn")
+
 func _on_Timer_timeout():
 	hide()
 	$CollisionShape2D.disabled = true
