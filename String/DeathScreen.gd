ab61bd960b9082bae90346775830c3d71e95f16b
Mia2b <mia2b@hotmail.com>
2019-02-18 10:49:58 -0700

    Clean deth

--- String/DeathScreen.gd
+++ String/DeathScreen.gd
@@ -4,5 +4,6 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	var random = randi()%8+1
 	
 	# get_tree().change_scene("res://MainMenu.tscn")
